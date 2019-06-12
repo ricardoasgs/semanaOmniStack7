5f278d5b3fcfabf8ce15cb82683d7c614c14d392
Ricardo Garcia <ricardoaugustogarcia@hotmail>
2019-06-12 14:49:49 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+mongoose.connect(
+  "mongodb+srv://admin:admin123@omnistack-7-b1hjw.mongodb.net/test?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+app.use(cors());
+
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
+
+app.use(require("./routes"));
+
+server.listen(3333);
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const multer = require("multer");
+const uploadConfig = require("./config/upload");
+const PostController = require("./controllers/PostController");
+const LikeController = require("./controllers/LikeController");
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+routes.get("/getposts", PostController.index);
+routes.post("/posts", upload.single("image"), PostController.store);
+
+routes.post("/post/:id/like", LikeController.like);
+
+module.exports = routes;
